faeba2d43041663339a32c0ed41d12a2c6cbe8c6
SapphireDisD <villagerlover52@hotmail.com>
2022-06-18 13:32:32 -0300

    Initial commit.

---
+++ Aeroplane/MainPage.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.ApplicationModel.Core;
+using control = Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Aeroplane
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            var coreTitleBar = CoreApplication.GetCurrentView().TitleBar;
+            coreTitleBar.ExtendViewIntoTitleBar = true;
+            coreTitleBar.LayoutMetricsChanged += CoreTitleBar_LayoutMetricsChanged;
+
+            Window.Current.SetTitleBar(CustomDragRegion);
+
+            NewTab(browserTabs);
+        }
+
+        private void CoreTitleBar_LayoutMetricsChanged(CoreApplicationViewTitleBar sender, object args)
+        {
+            if (FlowDirection == FlowDirection.LeftToRight)
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayRightInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayLeftInset;
+            }
+            else
+            {
+                CustomDragRegion.MinWidth = sender.SystemOverlayLeftInset;
+                ShellTitlebarInset.MinWidth = sender.SystemOverlayRightInset;
+            }
+
+            CustomDragRegion.Height = ShellTitlebarInset.Height = sender.Height;
+        }
+
+        // Add a new Tab to the TabView
+        private void TabView_AddTabButtonClick(control.TabView sender, object args)
+        {
+            NewTab(sender);
+        }
+
+        // Remove the requested tab from the TabView
+        private void TabView_TabCloseRequested(control.TabView sender, control.TabViewTabCloseRequestedEventArgs args)
+        {
+            sender.TabItems.Remove(args.Tab);
+            if(sender.TabItems.Count < 1) {
+                NewTab(sender);
+            }
+        }
+
+        private void NewTab(control.TabView sender) {
+            var newTab = new control.TabViewItem();
+            newTab.IconSource = new control.SymbolIconSource() { Symbol = Symbol.Document };
+            newTab.Header = "New Tab";
+
+            // The Content of a TabViewItem is often a frame which hosts a page.
+            Frame frame = new Frame();
+            frame.Navigate(typeof(WebViewPage), newTab);
+            newTab.Content = frame;
+
+            sender.TabItems.Add(newTab);
+
+            // Set the new tab as the selected tab
+            sender.SelectedItem = newTab;
+        }
+    }
+}
---
+++ Aeroplane/WebViewPage.xaml.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Web;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using control = Microsoft.UI.Xaml.Controls;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace Aeroplane
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class WebViewPage : Page
+    {   
+        public WebViewPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+            
+            control.TabViewItem tabViewItem = e.Parameter as control.TabViewItem;
+            webView.CoreWebView2Initialized += (sendero, eto) => {
+                sendero.CoreWebView2.DocumentTitleChanged += (sendert, ett) => {
+                    tabViewItem.Header = sendert.DocumentTitle;
+                };
+
+                sendero.CoreWebView2.SourceChanged += (sendert, ett) => {
+                    addressBar.Text = sendert.Source;
+                    backButton.IsEnabled = sendert.CanGoBack;
+                    forwardButton.IsEnabled = sendert.CanGoForward;
+                };
+
+                sendero.CoreWebView2.NavigationStarting += (sendert, ett) => {
+                    StackPanel stackPanel = new StackPanel();
+                    stackPanel.Orientation = Orientation.Horizontal;
+                    stackPanel.Children.Add(new control.ProgressRing() { Height = 11, Width = 11, Margin = new Thickness(0, 0, 9, 0) });
+                    stackPanel.Children.Add(new TextBlock() { Text = "Loading..." });
+
+                    tabViewItem.IconSource = null;
+                    tabViewItem.Header = stackPanel;
+
+                    reloadButton.Content = "\xE711";
+                };
+
+                sendero.CoreWebView2.NavigationCompleted += (sendert, ett) => {
+                    tabViewItem.Header = sendert.DocumentTitle;
+
+                    reloadButton.Content = "\xE72C";
+                };
+
+                sendero.CoreWebView2.FaviconChanged += async (sendert, ett) => {
+                    try {
+                        BitmapImage bitmapImage = new BitmapImage();
+                        await bitmapImage.SetSourceAsync((await sendert.GetFaviconAsync(0)));
+                        tabViewItem.IconSource = new control.ImageIconSource() { ImageSource = bitmapImage };
+                    } catch { }
+                };
+            };
+        }
+
+        private void TextBox_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter)
+            {
+                if(Uri.IsWellFormedUriString(((TextBox)sender).Text, UriKind.Absolute)) {
+                    webView.CoreWebView2.Navigate(((TextBox)sender).Text);
+                } else {
+                     webView.CoreWebView2.Navigate("https://www.bing.com/search?q=" + HttpUtility.UrlEncode(((TextBox)sender).Text));
+                }
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            webView.CoreWebView2.GoBack();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            webView.CoreWebView2.GoForward();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if(reloadButton.Content == "\xE72C") {
+                webView.CoreWebView2.Reload();
+            } else {
+                webView.CoreWebView2.Stop();
+            }
+        }
+    }
+}
